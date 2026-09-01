2a26ea50ae7a40fed79230fc2234bd454b0da24d
anikesh_pathak <anikeshpathak10@gmail.com>
2026-09-01 21:17:44 +0530

    Task T1.4: Project setup & architecture (frontend/src/main.jsx) [done]

---
+++ src/frontend/src/main.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import App from './App.jsx'
+import CreateCardPage from './pages/CreateCardPage.jsx'
+import CardViewPage from './pages/CardViewPage.jsx'
+import NotFoundPage from './pages/NotFoundPage.jsx'
+import './index.css'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App />}>
+          <Route index element={<CreateCardPage />} />
+          <Route path="card/:cardId" element={<CardViewPage />} />
+          <Route path="*" element={<NotFoundPage />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  </React.StrictMode>
+)
