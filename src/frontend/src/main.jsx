29152834c5ac16d10aa11467b9a4248eb14930cf
anikesh_pathak <anikeshpathak10@gmail.com>
2026-09-14 19:09:08 +0530

    Change request task CR15: Change request: frontend/src/main.jsx [done]

--- src/frontend/src/main.jsx
+++ src/frontend/src/main.jsx
@@ -4,8 +4,9 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import App from './App.jsx'
 import CreateCardPage from './pages/CreateCardPage.jsx'
 import CardViewPage from './pages/CardViewPage.jsx'
-import AdminDashboardPage from './pages/AdminDashboardPage.jsx'
 import NotFoundPage from './pages/NotFoundPage.jsx'
+import AdminLoginPage from './pages/AdminLoginPage.jsx'
+import AdminDashboardPage from './pages/AdminDashboardPage.jsx'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
@@ -14,8 +15,9 @@ ReactDOM.createRoot(document.getElementById('root')).render(
       <Routes>
         <Route path="/" element={<App />}>
           <Route index element={<CreateCardPage />} />
-          <Route path="admin" element={<AdminDashboardPage />} />
           <Route path="card/:id" element={<CardViewPage />} />
+          <Route path="admin" element={<AdminLoginPage />} />
+          <Route path="admin/dashboard" element={<AdminDashboardPage />} />
           <Route path="*" element={<NotFoundPage />} />
         </Route>
       </Routes>
